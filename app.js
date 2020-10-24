355a4aebd249828e537653b59d7bc6c74cb80f68
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-24 23:31:21 +0530

    port update

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post("/", function(req,res){
 app.post("/failure",function(req,res){
     res.redirect("/");
 });
-app.listen("3000",function(req,res){
+app.listen(process.env.PORT,function(req,res){
     console.log("Server is running on port 3000");
 });
 
